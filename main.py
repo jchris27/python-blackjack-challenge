c4aafd8b008c8cbc453326718b6f6341b397f320
Jin <53550425+jchris27@users.noreply.github.com>
2022-04-01 06:59:04 +0000

    fix bugs

--- main.py
+++ main.py
@@ -41,34 +41,39 @@ def deal_card():
   card = random.choice(cards)
   return card
   
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
-
-for _ in range(2): # _ is not need, but this for loop only needs to run twice
-  new_card = deal_card()
-  user_cards.append(new_card) #If you only want to add a single item (not a list) to an existing list, use append()
-  computer_cards.append(deal_card()) # Similar to above
-  
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input
 #and returns the score. 
 #Look up the sum() function to help you do this.
 
 def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards."""
   # return sum(cards)
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+  #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
   if sum(cards) == 21 and len(cards) == 2:
     return 0
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+  #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
   if 11 in cards and sum(cards) > 21:
     cards.remove(11)
     cards.append(1)
-    
   return sum(cards)
+  
+  #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+  user_cards = []
+  computer_cards = []
+  is_game_over = False
 
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+  for _ in range(2): # _ is not need, but this for loop only needs to run twice
+    new_card = deal_card()
+    user_cards.append(new_card) #If you only want to add a single item (not a list) to an existing list, use append()
+    computer_cards.append(deal_card()) # Similar to above
+
+  #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
+  print(f"Your cards: {user_cards}")
+  print(f"Computer's first card: {computer_cards[0]}")
+  if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over == True
 
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
